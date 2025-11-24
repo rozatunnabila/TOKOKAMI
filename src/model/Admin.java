23d37693c6182e9b15c7aba851401ece531b0702
Muhammad Al Hafiz <m.alhafiz761@gmail.com>
2025-11-25 03:51:39 +0700

    Admin.java

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -1,1 +1,23 @@
+package model;
 
+public class Admin extends User {
+    private String adminLevel;
+    
+    public Admin(String userId, String username, String password, String email, 
+                String phone, String address, String adminLevel) {
+        super(userId, username, password, email, phone, address);
+        this.adminLevel = adminLevel;
+    }
+    
+    @Override
+    public String getRole() {
+        return "ADMIN";
+    }
+    
+    @Override
+    public String getDisplayName() {
+        return "Admin " + username;
+    }
+    
+    public String getAdminLevel() { return adminLevel; }
+}
